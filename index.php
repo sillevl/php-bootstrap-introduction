008a305dce3164fda2bc181c2d05bb16f7d00c02
Sille Van Landschoot <info@sillevl.be>
2013-09-26 15:02:27 +0200

    added bootstrap css

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/boostrap-responsive.css">
 	<title></title>
 </head>
 <body>
